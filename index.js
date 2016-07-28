074942a4c4a7760b26dfe61bb49d68238b938990
Stefano Balietti <futur.dorko@gmail.com>
2016-07-28 12:20:37 -0400

    0.2.0

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+ * # nodegame-monitor
+ * Copyright(c) 2016 Stefano Balietti
+ * MIT Licensed
+ *
+ * Monitor files.
+ *
+ * http://www.nodegame.org
+ */
+
+var path = require('path');
+
+exports.publicDirPath = path.resolve(__dirname, 'public') + '/';
