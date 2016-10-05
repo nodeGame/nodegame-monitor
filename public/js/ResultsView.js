ee267845167973844da9fd24dd1a6365aea07f30
Stefano Balietti <futur.dorko@gmail.com>
2016-10-05 14:55:05 -0400

    better-results-view

--- public/js/ResultsView.js
+++ public/js/ResultsView.js
@@ -19,17 +19,55 @@
     // ## Meta-data
 
     ResultsView.version = '0.1.0';
-    ResultsView.description = 'Displays the results of games in data/ folder.'; 
+    ResultsView.description = 'Displays the results of games in data/ folder.';
 
     ResultsView.className = 'resultsView';
 
     // ## Dependencies
     ResultsView.dependencies = {
         JSUS: {},
+        Table: {}
     };
 
     function ResultsView(options) {
-        this.links = document.createElement('div');
+        var that;
+        that = this;
+
+        this.lastModified = null;
+
+        this.prefixLink = null;
+        this.prefixLink = window.location.origin;
+        this.prefixLink += W.uriChannel ? W.uriChannel : '/';
+        this.prefixLink += 'monitor/data/';
+
+        this.zipLink = null;
+
+        this.table = new W.Table({
+            render: {
+                pipeline: function(item) {
+                    var element, file;
+                    file = item.content.file;
+                    if (file) {
+                        element = document.createElement('a');
+                        element.setAttribute('target', '_blank');
+                        element.href = that.prefixLink + file;
+                        element.innerHTML = file;
+                        return element;
+                    }
+                    else if (item.content.mtime) {
+                        return document.createTextNode(item.content.mtime);
+                    }
+                },
+                returnAt: 'first'
+            }
+        });
+
+        this.table.setHeader(['File', 'Modified']);
+
+        // Creates table.table;
+        this.table.parse();
+
+        this.header = document.createElement('div');
     }
 
     ResultsView.prototype.refresh = function() {
@@ -45,44 +83,60 @@
     };
 
     ResultsView.prototype.append = function() {
-        this.bodyDiv.appendChild(this.links);
+        var b;
+        this.bodyDiv.appendChild(this.header);
+        
+        this.zipLink = document.createElement('a');
+        this.zipLink.setAttribute('target', '_blank');
+        this.zipLink.href = this.prefixLink + '*';
+        this.zipLink.innerHTML = '<em>Download all the content in a ' +
+            'zip archive</em>';
+        this.zipLink.style.display = 'none';
+
+        this.header.appendChild(this.zipLink);
+        this.header.appendChild(document.createElement('br'));
+
+        b = document.createElement('button');
+        b.innerHTML = 'Refresh';
+        b.onclick = this.refresh;
+        this.header.appendChild(b);
+
+        this.bodyDiv.appendChild(document.createElement('br'));
+        this.bodyDiv.appendChild(this.table.table);
+
         // Query server:
         this.refresh();
     };
 
     ResultsView.prototype.listeners = function() {
         var that;
-
         that = this;
 
-        // Listen for server reply:
+        // Listen for server reply.
         node.on.data('INFO_RESULTS', function(msg) {
+            that.receivedFiles = msg.data;
             that.displayData(msg.data);
         });
     };
 
     ResultsView.prototype.displayData = function(files) {
-        var i, element, dataDir, prefixLink;
-        prefixLink = window.location.origin;
-        prefixLink += W.uriChannel ? W.uriChannel : '/';
-        prefixLink += 'monitor/data/';
+        var i, element;
+        this.table.clear();
         if (files.length) {
-            element = document.createElement('a');
-            element.setAttribute('target', '_blank');
-            element.href = prefixLink + '*';
-            element.innerHTML = '<em>Download all the contents in a zip file</em>';
-            this.links.appendChild(element);
-            this.links.appendChild(document.createElement('br'));
-            this.links.appendChild(document.createElement('br'));
+            this.zipLink.style.display = '';
+            files.sort(function(a, b) {
+                if (a[0].file < b[0].file) return -1;
+                if (a[0].file > b[0].file) return 1;
+                return 0;
+            });
+            for (i = 0; i < files.length; ++i) {
+                this.table.addRow(files[i]);
+            }
         }
-        for (i = 0; i < files.length; ++i) {
-            element = document.createElement('a');
-            element.setAttribute('target', '_blank');
-            element.href = prefixLink + files[i];
-            element.innerHTML = files[i];
-            this.links.appendChild(element);
-            this.links.appendChild(document.createElement('br'));
+        else {
+            this.zipLink.style.display = 'none';
         }
+        this.table.parse();
     };
 
 })(node);
